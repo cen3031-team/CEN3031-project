64beb5e8a7929e3fa7f9bc4388a0fef651f2099d
Zeeshan Habib <zhabib@ufl.edu>
2019-02-26 22:11:35 -0500

    Initial sample web app commit

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+//This file holds any configuration variables we may need 
+//'config.js' is typically ignored by git to protect sensitive information, such as your database's username and password
+
+module.exports = {
+  db: {
+    uri: 'mongodb://zeeshanamin3:dbpassword123@ds139929.mlab.com:39929/software-eng-db', //place the URI of your mongo database here.
+  }, 
+  port: process.env.PORT || 8080
+};
---
+++ server/config/express.js
@@ -0,0 +1,39 @@
+var path = require('path'),  
+    express = require('express'), 
+    mongoose = require('mongoose'),
+    morgan = require('morgan'),
+    bodyParser = require('body-parser'),
+    config = require('./config'),
+    listingsRouter = require('../routes/listings.server.routes');
+
+module.exports.init = function() {
+  //connect to database
+  mongoose.connect(config.db.uri);
+
+  //initialize app
+  var app = express();
+
+  //enable request logging for development debugging
+  app.use(morgan('dev'));
+
+  //body parsing middleware 
+  app.use(bodyParser.json());
+
+  
+  /**
+  Serve static files */
+  app.use(express.static('client'));
+
+  /**
+  Use the listings router for requests to the api */
+  app.use('/api/listings', listingsRouter);
+
+  /**
+  Go to homepage for all routes not specified */ 
+  app.all('*', (req, res, next) => {
+    res.redirect('/');
+    next();
+  });
+
+  return app;
+};  
