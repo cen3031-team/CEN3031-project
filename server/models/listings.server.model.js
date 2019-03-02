535bd4b2133ba3fb7a87807a71ed11370fedf672
jedlai <jtedlai@gmail.com>
2019-03-01 20:04:05 -0500

    Adjusted server comments

--- server/models/listings.server.model.js
+++ server/models/listings.server.model.js
@@ -1,28 +1,27 @@
-/* Import mongoose and define any variables needed to create the schema */
 var mongoose = require('mongoose'), 
     Schema = mongoose.Schema;
 
-/* Create your schema */
+/* Schema definition */
 var listingSchema = new Schema({
   name: {
-    type: String, 
+    type: String,
     required: true
-  }, 
+  },
   code: {
-    type: String, 
-    required: true, 
+    type: String,
+    required: true,
     unique: true
-  }, 
-  address: String, 
+  },
+  address: String,
   coordinates: {
-    latitude: Number, 
+    latitude: Number,
     longitude: Number
   },
   created_at: Date,
   updated_at: Date
 });
 
-/* create a 'pre' function that adds the updated_at (and created_at if not already there) property */
+/*'pre' function that adds the updated_at (and created_at if not already there) property */
 listingSchema.pre('save', function(next) {
   var currentTime = new Date;
   this.updated_at = currentTime;
@@ -33,7 +32,7 @@ listingSchema.pre('save', function(next) {
   next();
 });
 
-/* Use your schema to instantiate a Mongoose model */
+/* Instantiate mongoose model using schema */
 var Listing = mongoose.model('Listing', listingSchema);
 
 /* Export the model to make it avaiable to other parts of your Node application */
