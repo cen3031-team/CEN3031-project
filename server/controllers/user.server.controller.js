292faaca0dfa1c2ee8de127209c64d7f53350dd6
Zeeshan Habib <zhabib@ufl.edu>
2019-03-22 13:26:46 -0400

    Added getUserbyUsername, getUserById, updateUser methods within controller and router

--- server/controllers/user.server.controller.js
+++ server/controllers/user.server.controller.js
@@ -13,3 +13,34 @@ exports.createUser = (req, res) => {
     }
   });
 }
+
+// Find user by username
+exports.getUserByUsername = (req, res) => {
+  User.find({username: req.params.username}, (err, user) => {
+    if (err) res.status(400).send(err);
+    res.json(user);
+  });
+}
+
+// Update user with new info
+exports.updateUser = (req, res) => {
+  const user = req.user;
+  const updatedUser = req.body.updatedUser;
+  user.first_name = updatedUser.first_name;
+  user.last_name = updatedUser.last_name;
+  user.password = updatedUser.password;
+
+  user.save((err) => {
+    if (err) res.status(400).send(err);
+    res.json(user);
+  });
+}
+
+// Middleware: Get User by ID
+exports.getUserById = (req, res, next ,id) => {
+  User.findById(id, (err, user) => {
+    if (err) res.status(400).send(err);
+    req.user = user;
+    next();
+  });
+}
