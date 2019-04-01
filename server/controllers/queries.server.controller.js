a6fa63095c3e31ede5b773c35989279a4356a411
Zeeshan Habib <zhabib@ufl.edu>
2019-03-31 20:12:45 -0400

    Added search bar, query frontend controller, tweet cards, user info etc etc for Query tool page

--- server/controllers/queries.server.controller.js
+++ server/controllers/queries.server.controller.js
@@ -1,29 +1,21 @@
 /* Dependencies */
 var request = require('request'),
-    config = require('../config/config');
-	
-exports.results = function(req, res) {
+  config = require('../config/config');
+
+exports.results = function (req, res) {
   // Configure req object
   const reqOptions = {
     method: 'GET',
-    url: 'https://api.twitter.com/1.1/search/tweets.json?q=' + req.params.queryId + '&result_type=popular&count=10',
+    url: 'https://api.twitter.com/1.1/search/tweets.json?q=' + req.params.queryId + '&result_type=popular&count=10&tweet_mode=extended',
     headers: {
       'Authorization': config.bearerToken
     }
   }
 
-  
-  console.log(reqOptions.url);
   request(reqOptions, (error, response, body) => {
     if (error) res.status(400).send(error);
     const queryArr = JSON.parse(body);
-	console.log(queryArr);
     res.json(queryArr);
   });
-  
-};
 
-exports.read = function(req, res) {
-  /* send back the listing as json from the request */
-  res.json(req.query);
 };
